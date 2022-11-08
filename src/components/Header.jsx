1d5bba87589f0c71495fa4dd3dca2a3aadfb3c8b
Filip Gudelj <filip.gudelj5@gmail.com>
2022-11-08 22:33:14 +0100

    First commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import "../styles/Header.scss";
+
+const Header = () => {
+  return (
+    <ul>
+      <li>Hello</li>
+      <li>Hello</li>
+      <li>Hello</li>
+      <li>Hello</li>
+    </ul>
+  );
+};
+
+export default Header;
